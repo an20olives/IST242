e47182f45a2204007fbe58dcdf97b95c5a88219b
an20olives <elitearo@gmail.com>
2020-09-10 14:39:43 -0400

    Add files via upload

---
+++ L01B_MVC/View/View.java
@@ -0,0 +1,17 @@
+package View;
+
+public class View
+{
+
+    public View()
+    {
+
+    }
+    
+    //Used to print our data to the console
+    public void basicDisplay(String s) // view likes strings, view doesnt know about data/model
+    {
+        System.out.println(s);
+    }
+
+}
