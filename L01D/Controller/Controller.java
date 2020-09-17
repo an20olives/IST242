3522cde3bd7e7a04fc0d4c602d711df2f4e34f84
an20olives <elitearo@gmail.com>
2020-09-17 14:35:13 -0400

    Testing ways for printing

--- L01D/Controller/Controller.java
+++ L01D/Controller/Controller.java
@@ -4,30 +4,27 @@ import Model.Model;
 import View.View;
 
 public class Controller {
-
+    
     Model model;
     View view;
-
+    
     public Controller(View v, Model m) {
         model = m;
         view = v;
 
         //Prints all Person from getData 
-        //view.basicDisplay(model.getData()); <<<< this methods "replaces" the 3 lines below
+        //view.basicDisplay(model.getData()); <<<< this methods "replaces" the lines below
+        /*
         view.basicDisplay(model.getPersons().get(0).toString());
         view.basicDisplay(model.getPersons().get(1).toString());
         view.basicDisplay(model.getPersons().get(2).toString());
-
+        view.basicDisplay(model.getPersons().get(3).toString());
+        view.basicDisplay(model.getPersons().get(4).toString());
+        */
         
-        /* Old basic display print from L01_C
         view.basicDisplay(model.getData(1));
-        view.basicDisplay(model.getData(2));
-        /...
-        //This one will be invalid
-        view.basicDisplay(model.getData(6));
         //Controller asks view to display information from model
         //................
-        */
     }
-
+    
 }
