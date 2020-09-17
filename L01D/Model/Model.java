320781c512a51787ec057eb7d05ab7b447792b8a
an20olives <elitearo@gmail.com>
2020-09-17 13:34:15 -0400

    Clean restart

--- L01D/Model/Model.java
+++ L01D/Model/Model.java
@@ -1,80 +1,69 @@
 package Model;
 
 import java.util.ArrayList;
-import java.util.Arrays;///
 
 public class Model {
 
-    //created new ArrayList for storing persons
-    private ArrayList<Person> persons;
+    //ArrayList attribute for Person, named persons
+    ArrayList<Person> persons;
 
     public Model() {
-        persons = new ArrayList<>();
+
+        //Initalize persons ArrayList under type Person
+        persons = new ArrayList<Person>();
         loadData();
     }
 
     public void loadData() {
-
-        // declare the Person' here
+        //Declare persons
         Person p1, p2, p3, p4, p5;
 
         //Person1 using all parameter constructor, also using new Height class
         p1 = new Person("Marcus Allen", 5, 2, 200, "Upper Marlboro, Md.", "Dr. Henry A. Wise, Jr.");
-        persons.add(p1); //adds p1 to the Person array
 
         //Person2 using all parameter constructor, also using new Height class
         p2 = new Person("Kyle Alston", 5, 9, 180, "Robbinsville, N.J.", "Robbinsville");
-        persons.add(p2); //adds p2 to the Person array
 
         //Person3 using all parameter constructor, also using new Height class
         p3 = new Person("Troy Apke", 6, 1, 220, "Mt. Lebanon, Pa.", "Mount Lebanon");
-        persons.add(p3); //adds p3 to the Person array
 
         //Person4 using all parameter constructor, also using new Height class
         p4 = new Person("Matthew Baney", 6, 0, 225, "State College, Pa.", "State College");
-        persons.add(p4); //adds p4 to the Person array
 
         //Person5 using no parameter constructor, also using new Height class
         p5 = new Person();
-        persons.add(p5); //adds p5 to the Person array
+        
+        //add persons to ArrayList persons
+        persons.add(p1);
+        persons.add(p2);
+        persons.add(p3);
+        persons.add(p4);
+        persons.add(p5);
     }
 
-    //getData(int n)
-    //getData()
-    //for loop
-    //return ArrayList<String>
-    //modified getData method for use with the array
-    //BOTH GETDATA WORK IN CRIPPLED STATE FIX IT
+
     public String getData(int n) {
-        ArrayList<String> data = new ArrayList<>();//maybe change all to data
-
-        //String s = persons.get(n).toString().trim();
-        //return s;
-        //return persons.get(n).toString();
-        data.add(persons.toString());
-
-        String stringData = "";
-        for (int i = 0; i < n; i++) {
-            stringData = data.get(i);
+        switch (n) { //using switch instead of if to return Persons(n).
+            case 1:
+                //return p1.toString();
+            case 2:
+                //return p2.toString();
+            case 3:
+                //return p3.toString();
+            case 4:
+                //return p4.toString();
+            case 5:
+                //return p5.toString();
+            default:
+                return ("invalid input parameter");
         }
-
-        return stringData;
     }
-
-    public ArrayList<String> getData() {
-        /*ArrayList<String> getData = new ArrayList<>();
-        for (int i = 0; i < persons.size(); i++) {
-            getData.add(persons.get(i).toString());
-            //getData.add("\n");
-        } */
-
-        ArrayList<String> data = new ArrayList<>();
-        data.add(persons.toString());
-
-        return data;
-    }
-
-    //get and set methods for the person array
+    
+    //public ArrayList<String> getData() {
+        
+    //}
+    
+    //get and set method for persons ArrayList
     public ArrayList<Person> getPersons() {
         return persons;
     }
@@ -82,5 +71,6 @@ public class Model {
     public void setPersons(ArrayList<Person> persons) {
         this.persons = persons;
     }
-
+    
+    //removed get and set methods from L01_C for individual person attributes
 }
