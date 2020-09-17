3c2dadee3e38773d1035be144a042092be3a5445
an20olives <elitearo@gmail.com>
2020-09-17 14:34:26 -0400

    For loop for printing getData (may change)

--- L01D/View/View.java
+++ L01D/View/View.java
@@ -1,18 +1,21 @@
 package View;
 
+import java.util.ArrayList;
+
 public class View {
 
     public View() {
 
     }
-    
+
     //Used to print our data to the console
     public void basicDisplay(String s) {// view likes strings, view doesnt know about data/model
         System.out.println(s);
     }
-    
-    //basicDisplay(ArrayList<String> arr){
-    //  for loop to display every line in the array
-    //}
 
+    public void basicDisplay(ArrayList<String> arr){
+        for (int i = 0; i < arr.size(); i++) {
+          System.out.println(arr.get(i));
+        }
+    }
 }
