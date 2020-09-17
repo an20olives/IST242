dbabec2546893011ea4cd1a212f9d77c15d4c9dd
an20olives <elitearo@gmail.com>
2020-09-17 13:35:03 -0400

    Clean restart

--- L01D/View/View.java
+++ L01D/View/View.java
@@ -1,7 +1,5 @@
 package View;
 
-import java.util.ArrayList;
-
 public class View {
 
     public View() {
@@ -9,18 +7,12 @@ public class View {
     }
     
     //Used to print our data to the console
-    
-    //GET THIS TO PRINT TO CONSOLE W NEW LINES BETWEEN EACH ARRAY ADDITION, SHOULD BE DONE HERE FIGURE IT OUT!
-    public void basicDisplay(ArrayList<String> arr) {
-        
-        int i;
-        for(i=0; i<5; i++){
-            System.out.println(arr);
-        }
-    }
-    
     public void basicDisplay(String s) {// view likes strings, view doesnt know about data/model
         System.out.println(s);
     }
+    
+    //basicDisplay(ArrayList<String> arr){
+    //  for loop to display every line in the array
+    //}
 
 }
