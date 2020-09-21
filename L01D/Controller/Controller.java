0757936662f36870bbb9c780be7391c6f3874726
an20olives <elitearo@gmail.com>
2020-09-20 21:03:26 -0400

    Final touches and comments added

--- L01D/Controller/Controller.java
+++ L01D/Controller/Controller.java
@@ -4,28 +4,27 @@ import Model.Model;
 import View.View;
 
 public class Controller {
-    
+
     Model model;
     View view;
-    
+
     public Controller(View v, Model m) {
         model = m;
         view = v;
 
         //Prints all Person from getData 
         view.basicDisplay(model.getData()); //<<<< this methods "replaces" the lines below
+        
+        //System.out.println(model.getData()); //direct test to see if array prints as full line '[]' present 
         /*
         view.basicDisplay(model.getPersons().get(0).toString());
         view.basicDisplay(model.getPersons().get(1).toString());
         view.basicDisplay(model.getPersons().get(2).toString());
         view.basicDisplay(model.getPersons().get(3).toString());
         view.basicDisplay(model.getPersons().get(4).toString());
-        */
-        
-        //System.out.println(model.persons.toString()); //test to see if things print as intended
-
+         */
         //Controller asks view to display information from model
         //................
     }
-    
+
 }
